3ed9018e944e1332267150b91e7f144986a1baa9
Vinesh-Gone <vinesh11vikram@gmail.com>
2025-03-03 14:56:04 +0530

    second

--- helloworld.java
+++ helloworld.java
@@ -1,1 +1,7 @@
-mvn archetype:generate -DgroupId=com.example -DartifactId=helloworld -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+class helloworld
+{
+  public static void main(String args[])
+{
+   System.out.println("welcome");
+}
+}
